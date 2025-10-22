d92893243ab3a3f487c27575895db59841a41edb
ntthuy18 <ntthuy188200@gmail.com>
2025-10-23 03:07:49 +0700

    hotfix: fix slow seeder db (#18)
    * hotfix: fix slow seeder db
    
    * fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Tắt foreign key checks để tăng tốc seeding
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         $this->call([
             AdminUserSeeder::class,  // tạo user trước
             CinemasSeeder::class,      // tạo cinema trước rooms
@@ -23,5 +27,8 @@ class DatabaseSeeder extends Seeder
             PointsHistorySeeder::class, // tạo lịch sử điểm
             ComboSeeder::class,      // tạo combo
         ]);
+
+        // Bật lại foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
