b84ac7be15769cacb8035af02ef60ef71aa0ed95
Turtle1811 <phongrua2002@gmail.com>
2025-12-04 01:52:04 +0700

    Fix thanh toan

--- config/vnpay.php
+++ config/vnpay.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'vnp_url'       => env('VNPAY_URL', 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html'),
-    'vnp_tmncode'   => env('VNPAY_TMNCODE'),
-    'vnp_hashsecret'=> env('VNPAY_HASHSECRET'),
+    'vnp_url' => env('VNPAY_URL'),
+    'vnp_tmncode' => "4FC0OH07", //Mã định danh merchant kết nối (Terminal Id)
+    'vnp_hashsecret' => "HA7LCGH11S2PM43UUPLOFB97BXS5PWHV", //Secret key
     'vnp_returnurl' => env('VNPAY_RETURN_URL'),
 ];
